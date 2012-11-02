afa8a461cd5130d4075e8cc1824f7eb7a2295c67
asoqa <asoqa51@gmail.com>
2012-11-02 17:23:21 +0800

    补充case

--- php/ft/core/TestIndex.php
+++ php/ft/core/TestIndex.php
@@ -3,7 +3,12 @@
 require_once 'PHPUnit\Framework\TestCase.php';
 
 /**
- * test case.
+ * elasticsearch.org:
+ * 	http://www.elasticsearch.org/guide/reference/api/index_.html
+ * 说明:
+ *   调用es restapi建立索引
+ * @author 	can.zhaoc
+ * 
  */
 class TestIndex extends PHPUnit_Framework_TestCase {
 	public static $ch;
@@ -72,6 +77,14 @@ class TestIndex extends PHPUnit_Framework_TestCase {
 		curl_close(self::$ch);
 	}
 	
+	/**
+	 * 说明：
+	 * 	在test索引index类型下建立id为1的索引，索引内容只有一个field
+	 * 前提：
+	 * 	建立好map，如setUpBeforeClass所示
+	 * 判断：
+	 * 	1.	返回的json索引
+	 */
 	public function testIndexWithId() {
 		$method = "PUT";
 		$id = 1;
@@ -93,5 +106,170 @@ class TestIndex extends PHPUnit_Framework_TestCase {
 		$expected = '{"ok":true,"_index":"test","_type":"index","_id":"1","_version":1}';
 		$this->assertEquals($expected, $result, $expected . " || " . $result);
 	}
+	
+	/**
+	 * 说明：
+	 * 	在test索引index类型下建立id为1的索引，索引内容多于map指定的field
+	 * 前提：
+	 * 	建立好map，如setUpBeforeClass所示
+	 * 判断：
+	 * 	1.	返回的json索引
+	 */
+	public function testIndexWithIdMoreFields() {
+		$method = "PUT";
+		$id = 2;
+		$url = "http://10.232.42.205/test/index/" . $id ;
+		self::$ch = curl_init($url);
+	
+		$index = '{
+		    "user" : "kimchy",
+		    "post_date" : "2009-11-15T14:12:12",				
+		    "message" : "trying out Elastic Search"
+		}';
+	
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		curl_setopt(self::$ch, CURLOPT_PORT, 9200);
+		curl_setopt(self::$ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt(self::$ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+		curl_setopt(self::$ch, CURLOPT_POSTFIELDS, $index);
+	
+		$result = curl_exec(self::$ch);
+	
+		$expected = '{"ok":true,"_index":"test","_type":"index","_id":"2","_version":1}';
+		$this->assertEquals($expected, $result, $expected . " || " . $result);
+	}	
+	
+	/**
+	 * Automatic index creation can be disabled by setting action.auto_create_index to false in the config file of all nodes. Automatic mapping creation can be disabled by setting index.mapper.dynamic to false in the config files of all nodes (or on the specific index settings).
+	 */
+	public function testIndexAutomatically() {
+		
+	}
+	
+	
+	/**
+	 * 说明：
+	 * 	通过version进行乐观并发锁控制，测试version自动累加
+	 * 前提：
+	 * 	建立好map，如setUpBeforeClass所示
+	 * 判断：
+	 * 	1.	返回的json索引,version自动+1
+	 */	
+	public function testIndexSetVersionAutomatically() {
+		$method = "PUT";
+		$id = 3;
+		$url = "http://10.232.42.205/test/index/" . $id;
+		self::$ch = curl_init($url);
+		
+		$index = '{
+		    "message" : "trying Elastic Search Version"
+		}';
+		
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		curl_setopt(self::$ch, CURLOPT_PORT, 9200);
+		curl_setopt(self::$ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt(self::$ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+		curl_setopt(self::$ch, CURLOPT_POSTFIELDS, $index);
+		
+		$result = curl_exec(self::$ch);
+		
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		$index = '{
+		    "message" : "trying Elastic Search Version again! version should be 2"
+		}';		
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		$result = curl_exec(self::$ch);		
+		
+		$expected = '{"ok":true,"_index":"test","_type":"index","_id":"3","_version":2}';
+		$this->assertEquals($expected, $result, $expected . " || " . $result);		
+	}
+
+	/**
+	 * 说明：
+	 * 	通过version进行乐观并发锁控制，当有多个版本时，可以指定最新版本更新
+	 * 前提：
+	 * 	建立好map，如setUpBeforeClass所示
+	 * 判断：
+	 * 	1.	返回的json索引,version自动+1
+	 */
+	public function testIndexUpdateLastVersion() {
+		$method = "PUT";
+		$id = 4;
+		$url = "http://10.232.42.205/test/index/" . $id;
+		self::$ch = curl_init($url);
+	
+		$index = '{
+		    "message" : "trying Elastic Search Version"
+		}';
+	
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		curl_setopt(self::$ch, CURLOPT_PORT, 9200);
+		curl_setopt(self::$ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt(self::$ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+		curl_setopt(self::$ch, CURLOPT_POSTFIELDS, $index);
+	
+		$result = curl_exec(self::$ch);
+	
+		for($i = 0; $i < 2; $i++) {
+			curl_setopt(self::$ch, CURLOPT_URL, $url);
+			$index = '{
+			    "message" : "trying Elastic Search Version again! version should be "' . ($i+2) . '
+			}';
+			curl_setopt(self::$ch, CURLOPT_URL, $url);
+			$result = curl_exec(self::$ch);
+		}
+	
+		$url = "http://10.232.42.205/test/index/" . $id . "/?version=3";
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		$index = '{
+		    "message" : "trying Elastic Search Version again! version should be 4"
+		}';
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		$result = curl_exec(self::$ch);
+		
+		$expected = '{"ok":true,"_index":"test","_type":"index","_id":"4","_version":4}';
+		$this->assertEquals($expected, $result, $expected . " || " . $result);
+	}	
+	
+	/**
+	 * 说明：
+	 * 	通过version进行乐观并发锁控制，当对非当前版本进行更新时，会产生冲突
+	 * 前提：
+	 * 	建立好map，如setUpBeforeClass所示
+	 * 判断：
+	 * 	1.	产生冲突，http 409错误
+	 */
+	public function testIndexVersionConflict() {
+		$method = "PUT";
+		$id = 5;
+		$url = "http://10.232.42.205/test/index/" . $id;
+		self::$ch = curl_init($url);
+		
+		$index = '{
+		    "message" : "trying Elastic Search Version"
+		}';
+		
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		curl_setopt(self::$ch, CURLOPT_PORT, 9200);
+		curl_setopt(self::$ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt(self::$ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+		curl_setopt(self::$ch, CURLOPT_POSTFIELDS, $index);
+		
+		$result = curl_exec(self::$ch);
+		
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		$index = '{
+		    "message" : "trying Elastic Search Version again! version should be 2"
+		}';
+		curl_exec(self::$ch);
+		
+		//制造冲突
+		$url = "http://10.232.42.205/test/index/" . $id . "/?version=1";
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		$result = curl_exec(self::$ch);
+		
+		$expected = '{"error":"VersionConflictEngineException[[test][1] [index][5]: version conflict, current [2], provided [1]]","status":409}';
+		$this->assertEquals($expected, $result, $expected . " || " . $result);		
+	}	
 }
 
